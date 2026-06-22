b0883489a51599842dd6f4c3b9cd98e537ca11fc
Imran Hasan <imranphero@gmail.com>
2026-06-22 13:08:09 +0600

    create a util function for price cacl

--- src/utils/formatPrice.ts
+++
@@ -1,5 +0,0 @@
-export const formatPrice = (price: number): string =>
-  new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price);
-
-export const calcDiscount = (original: number, current: number): number =>
-  Math.round(((original - current) / original) * 100);
